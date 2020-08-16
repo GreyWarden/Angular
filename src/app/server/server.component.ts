ee21940dc6c45560c0577992072a98b58c38a7b1
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-16 10:54:07 +0200

    Added String interpolation to server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  id = 1;
+  status = 'offline';
+}
