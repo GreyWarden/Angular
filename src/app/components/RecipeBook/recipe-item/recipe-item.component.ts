9ef66091ea48bdcccb2550fc6376926281707fc8
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-17 18:58:57 +0200

    Added basic html to components

--- src/app/components/RecipeBook/recipe-item/recipe-item.component.ts
+++ src/app/components/RecipeBook/recipe-item/recipe-item.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
-  constructor() { }
+  constructor () { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
   }
 
 }
