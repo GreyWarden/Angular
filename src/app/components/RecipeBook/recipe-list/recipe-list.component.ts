521952e954379f1d51cbd3a9d64348ec2b7d1e88
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-17 19:32:47 +0200

    Added styling and basic functionality to recipe list and recipe list item components

--- src/app/components/RecipeBook/recipe-list/recipe-list.component.ts
+++ src/app/components/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../Models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Salmorejo', 'Está to güeno, tt', 'https://www.eladerezo.com/wp-content/uploads/2008/08/salmorejo-cordobes-2048x1611.jpg'),
+    new Recipe('Gaspashiro', 'Pues un gaspasho como dioh manda', 'https://recetaslife.com/wp-content/uploads/2016/01/receta-de-gazpacho-extremeno.jpg')
+  ];
 
   constructor () { }
 
   ngOnInit (): void {
   }
-
 }
