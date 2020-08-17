6149bbb97730b751a3d134ea672f8e29156500ae
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-17 18:53:38 +0200

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/RecipeBook/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './components/Header/header/header.component';
+import { RecipesComponent } from './components/RecipeBook/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
