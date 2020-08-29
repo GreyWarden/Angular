07b141aee9a68aa18be11bb64ce674a5813fc6e5
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-29 10:46:22 +0200

    Modified AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {$e} from "codelyzer/angular/styles/chars";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  evenNumbers: number[] = [];
+  oddNumbers: number[] = [];
+  onNumberGiven(givenNumber: number): void {
+    givenNumber % 2 === 0 ? this.evenNumbers.push(givenNumber) : this.oddNumbers.push(givenNumber);
+  }
+
+  onNumbersReset(): void {
+    this.evenNumbers = [];
+    this.oddNumbers = [];
+  }
 }
