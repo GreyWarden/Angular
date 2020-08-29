0ba3ea7b1687df6c2e7bf1b940e452493b3d8c2c
Raúl Pavón <raulpavonfp@gmail.com>
2020-08-29 12:22:20 +0200

    ShoppingListEditComponent now emits an event using element references TODO: Remove element references and just use property binding

--- src/app/components/ShoppingList/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/ShoppingList/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Output, ViewChild, EventEmitter} from '@angular/core';
+import {Ingredient} from '../../Shared/ingredients/Models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('measurementSelect') measurementSelect: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  options: string[];
 
   constructor () { }
 
   ngOnInit (): void {
+    this.options = [
+      'pinch(es)',
+      'cl',
+      'spoon(s)',
+      'teaspoon(s)',
+      'piece(s)'
+    ];
   }
 
+  /*
+   *TODO: Implement validation and change way of getting the values for the ingredient
+   * right now is using ViewChild as a way to practice
+   */
+  addIngredient (event: Event): void {
+    event.preventDefault();
+    this.ingredientAdded.emit(new Ingredient(
+      this.nameInput.nativeElement.value,
+      this.amountInput.nativeElement.value,
+      this.measurementSelect.nativeElement.value
+    ));
+  }
 }
